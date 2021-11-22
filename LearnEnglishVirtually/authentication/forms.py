3e6ab324437244096e75a6243a1a0df6482a92e4
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-11-23 01:06:04 +0545

    Verify email if it already exists or not

--- LearnEnglishVirtually/authentication/forms.py
+++ LearnEnglishVirtually/authentication/forms.py
@@ -5,17 +5,24 @@ from django.utils.translation import  gettext_lazy as _
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm,AuthenticationForm,PasswordChangeForm,PasswordResetForm, UsernameField
 from django.core import validators
 
-def validete_username(value):
+def validate_username(value):
     if len(value)<=2:
         raise forms.ValidationError(f"Your username cannot be of {len(value)}  word")
+
+def validate_email(value):
+    print('Email error check')
+    email = User.objects.filter(email=value)
+    if email:
+        raise forms.ValidationError(f"This '{value}' email already exists!!!!!!! ")
+
 
 class UserCreation(UserCreationForm):
     password1 = forms.CharField(label="Password", widget = forms.PasswordInput(attrs={"placeholder":"Password",'autocomplete':'new-password','id':''}),error_messages={"required":"Please enter password"},)
     password2 = forms.CharField(label="Repeat your password",widget= forms.PasswordInput(attrs={"placeholder":"Repeat your password",'autocomplete':'new-password','id':''}),help_text="Make sure your password contains 'small letter','capital letter','numbers' and 'symbols'",error_messages={"required":"Re-Enter password field cannot be empty"})
-    username = forms.CharField(label="username",widget=forms.TextInput(attrs={"placeholder":"Username","id":"username",'class':'','id':'name'}),validators=[validete_username])
+    username = forms.CharField(label="username",widget=forms.TextInput(attrs={"placeholder":"Username","id":"username",'class':'','id':'name'}),validators=[validate_username])
     first_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"First Name","required":True,'id':'fname'}),error_messages={"required":"First name cannot be empty"})
     last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Last Name","required":True,'id':'lname'}),error_messages={"required":"Last name cannot be empty"})
-    email = forms.CharField(widget=forms.EmailInput(attrs={"required":True,"Placeholder":"Email",'autocomplete':'username','id':'email'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={"required":True,"Placeholder":"Email",'autocomplete':'username','id':'email'}),validators=[validate_email])
     class  Meta:
         model = User
         fields =['username','email','first_name','last_name','password1','password2']
