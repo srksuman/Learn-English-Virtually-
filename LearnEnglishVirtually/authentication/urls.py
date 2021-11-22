ed68b7117565180bcb6b3f92370c8b9457104ede
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-11-23 01:31:18 +0545

    Reset password form

--- LearnEnglishVirtually/authentication/urls.py
+++ LearnEnglishVirtually/authentication/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from .views import loginFunction,registerFunction,home,verifyUser
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/',loginFunction,name="login"),
     path('reg/',registerFunction,name="reg"),
     path('home/',home,name='home'),
     path('verify/',verifyUser,name="verify"),
+    # path('change-password/', auth_views.PasswordChangeView.as_view()),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+
 ]
