891bedf13a6e7abe8fe2c55d4d648ef44a7c5490
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2021-11-23 14:01:19 +0545

    Template page for password reset form added

--- LearnEnglishVirtually/authentication/urls.py
+++ LearnEnglishVirtually/authentication/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('verify/',verifyUser,name="verify"),
     # path('change-password/', auth_views.PasswordChangeView.as_view()),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='html/reset_password_form.html'),name='password_reset'), #submit email form
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'), # email sent success message for user
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='html/password_reset_done.html'),name='password_reset_done'), # email sent success message for user
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'), # link is sent to email to reset password
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'), #password successfully changed message
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='html/password_reset_complete.html'),name='password_reset_complete'), #password successfully changed message
 
 
 ]
