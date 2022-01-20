a4b23283dcd1fdd861062c4685dc8760f2405bf1
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-20 15:45:46 +0545

    Unit test for the form change password, update profile login form

--- LearnEnglishVirtually/authentication/test/test_forms.py
+++ LearnEnglishVirtually/authentication/test/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from authentication.forms import UserCreation, validate_username, validate_email
+from authentication.forms import UserCreation, validate_username, validate_email, LoginForm, VerifyForm, ChangePasswordUserForm,UserUpdateForm
+from django.contrib.auth.models import User
 
 class TestForms(TestCase):
     def test_filled_UserCreation(self):
@@ -23,7 +24,27 @@ class TestForms(TestCase):
         self.assertEqual(get_value,None)
 
     def test_validate_email(self):
-        get_value = validate_email('sumanrajkhanal@gmail.com')
+        get_value = validate_email('unnittesing@gmail.com')
         self.assertEqual(get_value,None)
 
-        
+    def test_LoginForm(self):
+        form = LoginForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),2)
+
+    def test_VerifyForm(self):
+        form = VerifyForm(data={})
+        fm = VerifyForm(data={'otp':12563})
+        self.assertTrue(fm.is_valid())
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),1)
+
+    def test_ChangePasswordUserForm(self):
+        form = ChangePasswordUserForm(User,data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),3)
+
+    def test_UserUpdateForm(self):
+        form = UserUpdateForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),3)
