dc21a53da2e239f470bd974e40b4e3dbb19ceb65
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-23 02:21:47 +0545

    main content and sub content table created

--- LearnEnglishVirtually/courses/admin.py
+++ LearnEnglishVirtually/courses/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MainCotent
+from .models import MainContent,SubContent
 # Register your models here.
 @admin.register(MainContent)
 class MainContentAdmin(admin.ModelAdmin):
-    list_display=['id','topic'.'sub_topic']
+    list_display=['id','topic']
+
+@admin.register(SubContent)
+class SubContentAdmin(admin.ModelAdmin):
+    list_display = ['id','topic','sub_topic']
--- LearnEnglishVirtually/courses/models.py
+++ LearnEnglishVirtually/courses/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class MainContent(models.Model):
     id = models.AutoField(primary_key=True)
     topic = models.CharField(max_length=200)
+    
+
+class SubContent(models.Model):
+    topic = models.ForeignKey(MainContent, on_delete=models.CASCADE)
     sub_topic = models.CharField(max_length=200)
+    content = models.TextField()
