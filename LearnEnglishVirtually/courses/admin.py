6f5d8e033fa48e86b2cb01061cfc065f14395ca6
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-23 01:41:21 +0545

    fields added in admin panel

--- LearnEnglishVirtually/courses/admin.py
+++ LearnEnglishVirtually/courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MainCotent
+# Register your models here.
+@admin.register(MainContent)
+class MainContentAdmin(admin.ModelAdmin):
+    list_display=['id','topic'.'sub_topic']
