e0e6daec66459d3eab5773e6aeea35eeb290a4df
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-23 01:39:48 +0545

    model creted for main content table

--- LearnEnglishVirtually/courses/models.py
+++ LearnEnglishVirtually/courses/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class MainContent(models.Model):
+    id = models.AutoField(primary_key=True)
+    topic = models.CharField(max_length=200)
+    sub_topic = models.CharField(max_length=200)
