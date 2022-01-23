262c412dc7d73dd6c05ba98be1dcc0e549547894
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-23 21:50:25 +0545

    added home,contact,about,courses pages

--- LearnEnglishVirtually/courses/urls.py
+++ LearnEnglishVirtually/courses/urls.py
@@ -3,4 +3,6 @@ from .views import *
 urlpatterns = [
     path('mainCourse/',mainCoursePage,name='mainCourse'),
     path('contactPage/',contactPage,name="contactPage"),
+    path('aboutPage/',aboutPage,name='aboutPage'),
+    path('courseContent/',courseContent,name='courseContent')
 ]
--- LearnEnglishVirtually/courses/views.py
+++ LearnEnglishVirtually/courses/views.py
@@ -5,6 +5,22 @@ def mainCoursePage(request):
     return render(request,'html/index.html')
 
 def contactPage(request):
-    pageName = "Contact"
-    
-    return render(request,'html/contact.html',{'pageName':pageName})
+    context = {
+    'TopPageName':"Contact Us",
+    'pageName':'Contact'
+    }
+    return render(request,'html/contact.html',context)
+
+def aboutPage(request):
+    context = {
+    'TopPageName':"About Us",
+    'pageName':'About'
+    }
+    return render(request,'html/about.html',context)
+
+def courseContent(request):
+    context = {
+    'TopPageName':"Our Courses",
+    'pageName':'Courses'
+    }
+    return render(request,'html/courses.html',context)
