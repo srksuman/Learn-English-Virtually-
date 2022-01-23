93fe3d6ea1594583579d8450985f12fae8d2d123
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-23 20:39:39 +0545

    templates files added for courses section

--- LearnEnglishVirtually/courses/models.py
+++ LearnEnglishVirtually/courses/models.py
@@ -1,14 +1,25 @@
 from django.db import models
-
+from django import forms
 # Create your models here.
 
 
 class MainContent(models.Model):
     id = models.AutoField(primary_key=True)
     topic = models.CharField(max_length=200)
+    def __str__(self):
+        return str(self.topic)
     
 
 class SubContent(models.Model):
     topic = models.ForeignKey(MainContent, on_delete=models.CASCADE)
     sub_topic = models.CharField(max_length=200)
     content = models.TextField()
+
+    def __str__(self):
+        return str(self.topic.topic)
+
+    def __unicode__(self):
+        return str(self.topic.topic)
+
+
+
--- LearnEnglishVirtually/courses/views.py
+++ LearnEnglishVirtually/courses/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import SubContent
 # Create your views here.
+def courseDisplay(request):
+    return render(request,'html/index.html')
