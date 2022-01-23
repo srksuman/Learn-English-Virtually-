09b5dfd83f29490dad700ee6bf96586a4677ffb8
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-23 21:10:33 +0545

    breadcrumbs

--- LearnEnglishVirtually/courses/urls.py
+++ LearnEnglishVirtually/courses/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from .views import *
 urlpatterns = [
-    path('course/',views.courseDisplay,name='courseDisplay'),
+    path('mainCourse/',mainCoursePage,name='mainCourse'),
+    path('contactPage/',contactPage,name="contactPage"),
 ]
--- LearnEnglishVirtually/courses/views.py
+++ LearnEnglishVirtually/courses/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import SubContent
 # Create your views here.
-def courseDisplay(request):
+def mainCoursePage(request):
     return render(request,'html/index.html')
+
+def contactPage(request):
+    pageName = "Contact"
+    
+    return render(request,'html/contact.html',{'pageName':pageName})
