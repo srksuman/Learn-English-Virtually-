76b138490452b3825240c74a74ec25aef789a43b
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-27 03:20:51 +0545

    ckeditor added

--- LearnEnglishVirtually/LearnEnglishVirtually/settings.py
+++ LearnEnglishVirtually/LearnEnglishVirtually/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication',
     'courses',
-    'chatbot'
+    'chatbot',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
--- LearnEnglishVirtually/courses/models.py
+++ LearnEnglishVirtually/courses/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django import forms
 from autoslug import AutoSlugField
+from ckeditor.fields import RichTextField
 # Create your models here.
 class MainContent(models.Model):
     id = models.AutoField(primary_key=True)
     topic = models.CharField(max_length=200)
-    content = models.TextField(null=True,blank=True)
+    content = RichTextField(null=True,blank=True)
     estimated_time = models.FloatField(null=True,blank=True,default=1)
     sub_topic = models.CharField(max_length=200,null=True,blank=True)
     slug = AutoSlugField(populate_from='topic',unique=True,null=True,blank=True,default=None)
@@ -14,10 +15,10 @@ class MainContent(models.Model):
         return str(self.topic)
     
 
-# class SubContent(models.Model):
+class SubContent(models.Model):
 #     topic = models.ForeignKey(MainContent, on_delete=models.CASCADE)
-#     sub_topic = models.CharField(max_length=200)
-#     content = models.TextField()
+    sub_topic = models.CharField(max_length=200)
+    content = RichTextField()
 
 #     def __str__(self):
 #         return str(self.topic.topic)
