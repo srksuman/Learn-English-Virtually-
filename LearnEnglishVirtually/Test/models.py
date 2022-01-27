0389156ba825610d4dae7c065d7fefe2d104c095
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-27 17:03:36 +0545

    models added for test management and questions

--- LearnEnglishVirtually/Test/models.py
+++ LearnEnglishVirtually/Test/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from courses.models import MainContent
+# Create your models here.s
 
-# Create your models here.
+class TestManagement(models.Model):
+    topic = models.ForeignKey(MainContent,on_delete=models.CASCADE)
+    number_of_question = models.IntegerField()
+    time = models.FloatField(help_text="Duration of the test in minutes")
+    pass_marks = models.FloatField(help_text="Passmarks for test")
+
+    def __str__(self):
+        return self.topic
+
+    def get_questions(self):
+        pass
+
+    
+class Questions(models.Model):
+    text = models.CharField(max_length=200)
+    test = models.ForeignKey(TestManagement,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.text
+
+    def get_answers(self):
+        pass
+    
