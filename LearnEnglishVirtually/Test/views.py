069f1d67a85bfb96ea6f5415acb95cb4cce0c888
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-29 22:27:44 +0545

    test management list view class added

--- LearnEnglishVirtually/Test/views.py
+++ LearnEnglishVirtually/Test/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import JsonResponse
 from .models import *
-# from django.views.generic import ListView
-# # Create your views here.
 
-# class TestManagementList(ListView):
-#     model = TestManagement
-#     # context_object_name = ''
-#     template_name='html/main.html'
-
-# def test_view(request,id):
-#     test = TestManagement.objects.get(pk=id)
-#     return render(request,'',{'obj':test})
-
-
-# def testContent(request):
-#     test = TestManagement.objects.all()
-#     return render(request,'html/testContent.html',{'test':test})
+class TestManagementListView(ListView):
+    model = TestManagement 
+    template_name = 'html/main.html'
