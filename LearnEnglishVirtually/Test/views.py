cb396334ddec46599eccb0f005a0b4ef330cf1b6
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-01-29 22:29:38 +0545

    test management details view

--- LearnEnglishVirtually/Test/views.py
+++ LearnEnglishVirtually/Test/views.py
@@ -10,3 +10,16 @@ class TestManagementListView(ListView):
 def test_management_view(request, pk):
     testMgt = TestManagement.objects.get(pk=pk)
     return render(request, 'quiz.html', {'obj': testMgt})
+
+def test_details_view(request, pk):
+    quiz = Quiz.objects.get(pk=pk)
+    questions = []
+    for q in quiz.get_questions():
+        answers = []
+        for a in q.get_answers():
+            answers.append(a.text)
+        questions.append({str(q): answers})
+    return JsonResponse({
+        'data': questions,
+        'time': quiz.time,
+    })
