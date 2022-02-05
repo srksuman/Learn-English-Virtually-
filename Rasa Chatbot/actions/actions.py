87333fda10ff76ec4f6e6034208fb20d9a8e498a
Suman Raj Khanal <sumanrajkhanal@gmail.com>
2022-02-06 00:37:31 +0545

    sssdsf

--- Rasa Chatbot/actions/actions.py
+++ Rasa Chatbot/actions/actions.py
@@ -7,21 +7,25 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+import json
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        url = "https://apiforenglish.herokuapp.com/check/i is bad"
+        x = requests.get(url)
+        print(json.loads(x.text)['output'])
+        print("suman raj khanal -->",tracker.latest_message['text'])
+        dispatcher.utter_message(text=json.loads(x.text)['output'])
+
+        return []
